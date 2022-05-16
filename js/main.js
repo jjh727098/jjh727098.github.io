a7d7147d514df01ab479711596e413fc5ce69f3e
jjh727098 <jjh727098@naver.com>
2022-05-16 21:01:28 +0900

    메인 헤더 js 추가

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    /* -------------------------- header --------------------------- */
+    let $header = $('.page-header');
+    let headerOst = $header.offset().top;
+
+    let menu = $header.find('nav>ul>li'),
+		headerHeight =  $header.outerHeight(),
+		newHeight = 0,
+		subMenu = menu.find('ul');
+    
+    $(window).scroll(function(){
+        let sct = $(this).scrollTop();
+        if(sct > headerOst){
+            $header.addClass('active');
+        }else{
+            $header.removeClass('active');
+        }
+    });
+
+
+
+}); //ready
+    
