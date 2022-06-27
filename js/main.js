23d0904e90a76e7c4d778fc1edbe542b828dbbbb
jjh727098 <jjh727098@naver.com>
2022-06-27 17:48:04 +0900

    쿠키 활용 팝업 수정중

--- js/main.js
+++ js/main.js
@@ -206,7 +206,72 @@ $(function(){
     //     responsive: true
 	// });
 
- /* ------------------------- land --------------------------- */
+/*---------------------------- popup -------------------------*/
+let popup = $('.modal_box');
+        let popupCheckBox = $('#intro_modal_close');
+        let popupClose = popup.find('.intro_modal_close');
 
+        //쿠키 생성 함수
+        function setCookie(name,value,day){
+            let date = new Date();
+            date.setDate(date.getDate() + day);
+
+            let cookieContent = '';
+            cookieContent += `${name}=${value};`;
+            cookieContent += `Expires=${date.toUTCString()}`;            
+
+            $(document).cookie = cookieContent;
+        }
+        /*
+        쿠키체크 
+            쿠키 있다면 - 팝업이 안보인다.
+            쿠키 없다면 - 팝업이 보인다.
+
+        닫기 버큰을 클릭하면 할일
+            하루안보기 체크안하고 닫으면 - 쿠키지운다.
+            체크하고 닫으면 - 쿠키생성
+        */
+
+
+        //쿠키 확인 함수
+        function getCookie(name){
+            let visited = false;
+            let cookies = $(document).cookie.string.split(';'); //문자열 ; 구분해서 배열 생성
+
+            for(let cookie of cookies){
+                if(cookie.indexOf(name) > -1){
+                    visited = true;
+                }
+            }
+            if(visited){
+                popup.css({display:'none'}); //재방문
+            }else{
+                popup.css({display:'block'}); //첫방문, 안보기 체크안하고 닫기,
+            }
+        }        
+        getCookie('ABC');
+
+        //쿠키 삭제 함수
+        function delCookie(name,value){           
+
+            let date = new Date();
+            date.setDate(date.getDate() - 1);
+
+            let cookieContent = '';
+            cookieContent += `${name}=${value};`;
+            cookieContent += `Expires=${date.toUTCString()}`;            
+
+            $(document).cookie = cookieContent;
+        }            
+
+
+        popupClose.click(function(){
+            popup.css({display:'none'});
+            if(popupCheckBox.prop('checked',true)){ //체크되었다면, 팝업을 다시 안보겠다, 쿠키생성
+                setCookie('ABC','Main Page',1);
+            }else{//체크x, 팝업을 다시 보겠다, 쿠키제거
+                delCookie('ABC','Main Page');
+            }
+        });
 
 }); //ready
